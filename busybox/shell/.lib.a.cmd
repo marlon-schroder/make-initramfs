782776cb8a9ccba0516f4f27a876190909251aa5
Marlon Schroder <marlon.schroder@gmail.com>
2020-01-25 00:45:59 -0300

    upgrade

---
+++ busybox/shell/.lib.a.cmd
@@ -0,0 +1,1 @@
+cmd_shell/lib.a := rm -f shell/lib.a; ar  rcs shell/lib.a shell/hush.o shell/match.o shell/shell_common.o
---
+++ busybox/util-linux/.lib.a.cmd
@@ -0,0 +1,1 @@
+cmd_util-linux/lib.a := rm -f util-linux/lib.a; ar  rcs util-linux/lib.a util-linux/findfs.o util-linux/losetup.o util-linux/mount.o util-linux/switch_root.o
